3b9c9e4b99a85e196d46dcc6aa0a34cd27ca03eb
Jessica West <jessicawest@Jessicas-MacBook-Pro.local>
2021-12-29 14:31:56 -0800

    add shawn

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Image from 'next/image'
+import shawnPic from '../public/shawn-mccarthy.jpeg'
 
 export default function Home() {
   return (
@@ -11,13 +13,14 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to Shawn's page!" />
+        <Header title="Countdown to my flippie floppies" />
         
+	 <Image src={shawnPic} alt="shawn-mug"/>
         <p className="description">
-         <img src="/public/shawn-mccarthy.jpeg" alt="shawn-mug" className={styles.image}>
          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
          <div class="elfsight-app-7d29a667-4e41-4726-b0dc-419cff577c8e"></div>
         </p>
+
       </main>
 
       <Footer />
