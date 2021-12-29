aaf9e230f4451ea3397c4e1e796af98e7e65437b
Jessica West <jgoulding2@gmail.com>
2021-12-29 13:49:20 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
         <Header title="Welcome to Shawn's page!" />
         
         <p className="description">
+         <img src="/public/shawn-mccarthy.jpeg" alt="shawn-mug" className={styles.image}>
          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
          <div class="elfsight-app-7d29a667-4e41-4726-b0dc-419cff577c8e"></div>
         </p>
