a3729e276245e1cbc3955e1d11f758ce0ebccba5
Jessica West <jgoulding2@gmail.com>
2021-12-29 12:57:52 -0800

    add favicon and image

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Shawn Retirement Countdown</title>
+        <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/apple/285/weary-cat_1f640.png" />
       </Head>
 
       <main>
         <Header title="Welcome to Shawn's page!" />
+        <img src="https://avatars.githubusercontent.com/u/12153226?v=4">
         <p className="description">
          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
          <div class="elfsight-app-7d29a667-4e41-4726-b0dc-419cff577c8e"></div>
